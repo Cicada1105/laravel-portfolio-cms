a6dc1d79a137d036437a74744b965a6f23d15a85
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2022-08-09 00:01:05 -0400

    Exposed database data through API

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Models\Type;
 use App\Models\User;
 use App\Models\Project;
+use App\Models\Education;
+use App\Models\Employment;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,13 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/types', function(){
-
-    $types = Type::orderBy('title')->get();
-    return $types;
-
-});
-
 Route::get('/projects', function(){
 
     $projects = Project::orderBy('created_at')->get();
@@ -36,7 +30,6 @@ Route::get('/projects', function(){
     foreach($projects as $key => $project)
     {
         $projects[$key]['user'] = User::where('id', $project['user_id'])->first();
-        $projects[$key]['type'] = Type::where('id', $project['type_id'])->first();
 
         if($project['image'])
         {
@@ -62,3 +55,46 @@ Route::get('/projects/profile/{project?}', function(Project $project){
 
 });
 
+
+Route::get('/education', function(){
+
+    $institutions = Education::orderBy('created_at')->get();
+
+    foreach($institutions as $key => $education)
+    {
+        $institutions[$key]['user'] = User::where('id', $education['user_id'])->first();
+    }
+
+    return $institutions;
+
+});
+
+Route::get('/education/profile/{education?}', function(Education $education){
+
+    $education['user'] = User::where('id', $education['user_id'])->first();
+
+    return $education;
+
+});
+
+
+Route::get('/employment', function(){
+
+    $employments = Education::orderBy('created_at')->get();
+
+    foreach($employments as $key => $employment)
+    {
+        $employments[$key]['user'] = Employment::where('id', $employment['user_id'])->first();
+    }
+
+    return $employments;
+
+});
+
+Route::get('/employment/profile/{employment?}', function(Employment $employment){
+
+    $employment['user'] = User::where('id', $employment['user_id'])->first();
+
+    return $employment;
+
+});
